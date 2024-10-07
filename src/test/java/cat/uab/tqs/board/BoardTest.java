8a160d23598e2220d42304b0d29a903ffd8b687d
Arnau <arnaumunozbarrera@gmail.com>
2024-10-07 13:27:25 +0200

    Add tests

---
+++ src/test/java/cat/uab/tqs/board/BoardTest.java
@@ -0,0 +1,14 @@
+package test.java.cat.uab.tqs.board;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BoardTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
