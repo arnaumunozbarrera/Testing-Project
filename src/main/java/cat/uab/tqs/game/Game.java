c1e672937e2657e927ff76d868d4129560364105
Arnau <arnaumunozbarrera@gmail.com>
2024-10-13 11:18:53 +0200

    Add class game & initial menu

---
+++ src/main/java/cat/uab/tqs/game/Game.java
@@ -0,0 +1,44 @@
+package main.java.cat.uab.tqs.game;
+
+public class Game {
+    // Matrix of positions of the game
+	
+ 	// W represents water
+ 	// S represents ship
+ 	// H represents ship hitted
+ 	
+    // Board for Players to hit the ships
+     public static String[][] gameP1 = {
+    	 {"", "", "", ""},
+	     {"", "", "", ""},
+	     {"", "", "", ""},
+	     {"", "", "", ""},
+	     {"", "", "", ""}
+     };
+     
+	 public static String[][] gameP2 = {
+		 {"", "", "", ""},
+	     {"", "", "", ""},
+	     {"", "", "", ""},
+	     {"", "", "", ""},
+	     {"", "", "", ""}
+	 };
+
+   
+    // Board for Players to place the ships
+    public static String[][] boardP1 = {
+        {"", "", "", ""},
+        {"", "", "", ""},
+        {"", "", "", ""},
+        {"", "", "", ""},
+        {"", "", "", ""}
+    };
+    
+    public static String[][] boardP2 = {
+        {"", "", "", ""},
+        {"", "", "", ""},
+        {"", "", "", ""},
+        {"", "", "", ""},
+        {"", "", "", ""}
+    };
+}
