54de05acc734baa68411704f0e9d3b12845c18c3
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2024-11-26 19:00:17 +0100

    Add tests ShipModel

--- src/test/java/cat/uab/tqs/model/ShipModelTest.java
+++ src/test/java/cat/uab/tqs/model/ShipModelTest.java
@@ -4,21 +4,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.java.cat.uab.tqs.model.ShipModel;
+
 class ShipModelTest {
 
 	@Test
-	void testShipModel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	void testGetRow() {
-		fail("Not yet implemented");
+	    ShipModel s1 = new ShipModel(0, 0); // valor límit inferior
+	    assertEquals(s1.getRow(), 0);
+	    
+	    ShipModel s2 = new ShipModel(7, 7); // valor límit superior
+	    assertEquals(s2.getRow(), 7);
+
+	    ShipModel s3 = new ShipModel(4, 4); // valor mitjà
+	    assertEquals(s3.getRow(), 4);
+
+	    ShipModel s4 = new ShipModel(3, 5); // partició dins dels límits
+	    assertEquals(s4.getRow(), 3);
+	    
+	    ShipModel s5 = new ShipModel(1, 7); // partició dins dels límits
+	    assertEquals(s5.getRow(), 1);
+
+	    // Casos fora de límit no serien vàlids per una matriu de 8x8 i s'han d'evitar en aquest context.
 	}
 
 	@Test
 	void testGetCol() {
-		fail("Not yet implemented");
+		ShipModel s1 = new ShipModel(0, 0); // valor límit inferior
+	    assertEquals(s1.getCol(), 0);
+	    
+	    ShipModel s2 = new ShipModel(7, 7); // valor límit superior
+	    assertEquals(s2.getCol(), 7);
+
+	    ShipModel s3 = new ShipModel(4, 4); // valor mitjà
+	    assertEquals(s3.getCol(), 4);
+
+	    ShipModel s4 = new ShipModel(3, 5); // partició dins dels límits
+	    assertEquals(s4.getCol(), 3);
+	    
+	    ShipModel s5 = new ShipModel(1, 7); // partició dins dels límits
+	    assertEquals(s5.getCol(), 1);
+
+	    // Casos fora de límit no serien vàlids per una matriu de 8x8 i s'han d'evitar en aquest context.
 	}
 
 }
