3b22c42269967e66793a157356586c668f9e08df
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2024-11-27 19:38:14 +0100

    Update tests with MockObject in MessageView

---
+++ src/main/java/cat/uab/tqs/view/MockMessageView.java
@@ -0,0 +1,21 @@
+package main.java.cat.uab.tqs.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockMessageView extends MessageView {
+    private final List<String> messages = new ArrayList<>();
+
+    @Override
+    public void showMessage(String message) {
+        messages.add(message);
+    }
+
+    public List<String> getMessages() {
+        return messages;
+    }
+
+    public void clearMessages() {
+        messages.clear();
+    }
+}
