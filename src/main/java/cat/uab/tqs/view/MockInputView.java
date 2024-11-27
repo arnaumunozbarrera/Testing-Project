7c7081d73a634d98209ac273f43aa11e56ffe5b6
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2024-11-27 19:36:17 +0100

    Update tests with MockObject in InputView

---
+++ src/main/java/cat/uab/tqs/view/MockInputView.java
@@ -0,0 +1,19 @@
+package main.java.cat.uab.tqs.view;
+
+public class MockInputView extends InputView {
+    private final int[] inputs;
+    private int index = 0;
+
+    public MockInputView(int[] inputs) {
+        this.inputs = inputs;
+    }
+
+    @Override
+    public int getIntInput(String prompt) {
+        if (index < inputs.length) {
+            return inputs[index++];
+        } else {
+            throw new RuntimeException("No more inputs available in MockInputView.");
+        }
+    }
+}
