32d83a4cc79c000ea051bba75444cbe0fd89bc99
PepOrtinLopz <peportinlo@gmail.com>
2024-11-28 18:00:52 +0100

    Update BoardModel and BoardModelTest

--- src/test/java/cat/uab/tqs/model/BoardModelTest.java
+++ src/test/java/cat/uab/tqs/model/BoardModelTest.java
@@ -4,55 +4,39 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import main.java.cat.uab.tqs.model.BoardModel;
+
 class BoardModelTest {
-
-	private BoardModel board = new BoardModel(8, '~', 'S', 'X', 'O');
 	
-	
-	@Test
-	void testBoardModel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetSize() {
-		assertEquals(8,board.getSize());
-	}
-
-	@Test
-	void testGetCell() {
-		fail("Not yet implemented");
-	}
+	private BoardModel board = new BoardModel(4, '~', 'S', 'X', 'O');
 
 	@Test
 	void testSetCell() {
 		
 		// row: vàlid i col: vàlid
-		assertTrue(board.setCell(2, 2, 'S'));
+		assertTrue(board.setCell(2, 2,board.getSize(), 'S'));
 		
 		// row: vàlid i col: invàlid
-		assertFalse(board.setCell(2, 9, 'S'));
+		assertFalse(board.setCell(2, 9,board.getSize(), 'S'));
 		
 		// row: invàlid i col: vàlid
-		assertFalse(board.setCell(9, 2, 'S'));
+		assertFalse(board.setCell(9, 2,board.getSize(), 'S'));
 		
 		// row: invàlid i col: invàlid
-		assertFalse(board.setCell(9, 9, 'S'));
+		assertFalse(board.setCell(9, 9,board.getSize(), 'S'));
 	}
 	
 	@Test
 	void testIsBetween() {
-		//particions equivalents de row: -4,0,1,2,4,7,8,9,14
-		assertFalse(board.isBetween(-4));
-		assertFalse(board.isBetween(0));
-		assertTrue(board.isBetween(1)); 
-		assertTrue(board.isBetween(2));
-		assertTrue(board.isBetween(4));
-		assertTrue(board.isBetween(7));
-		assertTrue(board.isBetween(8));
-		assertFalse(board.isBetween(9));
-		assertFalse(board.isBetween(14));
-		
+		//Teniendo en cuenta una matriz de 4x4, particions equivalents de row: -4,0,1,2,3,4,5,8
+		assertFalse(board.isBetween(-4,board.getSize()));
+		assertFalse(board.isBetween(0,board.getSize()));
+		assertTrue(board.isBetween(1, board.getSize())); 
+		assertTrue(board.isBetween(2, board.getSize()));
+		assertTrue(board.isBetween(3, board.getSize()));
+		assertTrue(board.isBetween(4, board.getSize()));
+		assertFalse(board.isBetween(5, board.getSize()));
+		assertFalse(board.isBetween(8, board.getSize()));
 	}
 
 	@Test
@@ -61,22 +45,18 @@ class BoardModelTest {
 		assertTrue(board.isCellEmpty(1, 1));
 		
 		//Celda (2,2) --> ship
-		board.setCell(2, 2, 'S');
+		board.setCell(2, 2, board.getSize(), 'S');
 		assertFalse(board.isCellEmpty(2, 2));
 		
 		//Celda (3,3) --> hit
-		board.setCell(3, 3, 'X');
+		board.setCell(3, 3,board.getSize(), 'X');
 		assertFalse(board.isCellEmpty(3, 3));
 		
 		//Celda (4,4) --> miss
-		board.setCell(4, 4, '0');
-		assertFalse(board.isCellEmpty(4, 4));
+		board.setCell(3, 3,board.getSize(), 'O');
+		assertFalse(board.isCellEmpty(3, 3));
 	}
 
-	@Test
-	void testGetGrid() {
-		fail("Not yet implemented");
-	}
 
 	@Test
 	void testGetShipChar() {
@@ -93,5 +73,5 @@ class BoardModelTest {
 		assertEquals('O',board.getMissChar());
 	}
 
-
 }
+
