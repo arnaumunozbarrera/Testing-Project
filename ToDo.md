f58379cd4a8355b9d61cfc276d8d3a5f2b871fcf
Arnau Muñoz Barrera <arnaumunozbarrera@gmail.com>
2024-11-29 18:34:02 +0100

    Update ToDo & Main

--- ToDo.md
+++ ToDo.md
@@ -1,31 +1,26 @@
 ﻿----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 · To Do: 
 
-- MVC
 - TDD: 2 versions
 	Falta en: BattleShipControllerTest, BoardModelTest, ShipModelTest, BoardViewTest, InputViewTest & MessageViewTest
-
-- Caja negra: Per a tots els mètodes desenvolupats de classes de baix nivell:
-	90 % de totes les particions equivalents
-	90% de tots els casos possibles de valors límit i frontera
 
    Per a un mètode desenvolupat:
 	Pairwise testing
 	Falta: BoadModel
 
 Caixa Blanca:
-   Per a tot el codi:
    Per a un mínim de 3 mètodes desenvolupats:
  	Decision coverage, condition coverage
    Per a un mínim de dos mètodes desenvolupats:
 	Path coverage
    Per a un mínim de dos loops simples i dos loops aniuats:
  	Loop testing
-	Falta en: BoardView
+	Falta en: 1 loop simple y los loops anidados
 
 
 - Mock object:
-	A més dels mock object necessaris per poder test del Model i el Controlador simulant la interacció amb la Vista, un mínim de quatre mock object més. Dos d’ells han d’estar fets amb mockito i els altres dos implementats per vosaltres directament en codi (és a dir, sense utilitzar mockito).
+	A més dels mock object necessaris per poder test del Model i el Controlador simulant la interacció amb la Vista, un mínim de quatre mock object més. 
+	Dos d’ells han d’estar fets amb mockito i els altres dos implementats per vosaltres directament en codi (és a dir, sense utilitzar mockito).
 	Falta: repasar
 
 - CI:
--- src/main.java
+++ src/main.java
@@ -1,3 +1,4 @@
+
 import main.java.cat.uab.tqs.controller.BattleshipController;
 import main.java.cat.uab.tqs.model.BoardModel;
 import main.java.cat.uab.tqs.view.BoardView;
@@ -6,8 +7,9 @@ import main.java.cat.uab.tqs.view.MessageView;
 
 public class main {
     public static void main(String[] args) {
-        int boardSize = 4;
-        int shipCount = 2;
+    	
+        int boardSize = 8; // Board size 8x8 
+        int shipCount = boardSize/2; // Ship number 4
         char water = '-';
         char ship = 's';
         char hit = 'X';
@@ -21,10 +23,12 @@ public class main {
         InputView inputView2 = new InputView();
         MessageView messageView1 = new MessageView();
         MessageView messageView2 = new MessageView();
-
+        
         BattleshipController player1Controller = new BattleshipController(board1, boardView1, inputView1, messageView1, "Player 1 (P1)");
         BattleshipController player2Controller = new BattleshipController(board2, boardView2, inputView2, messageView2, "Player 2 (P2)");
-
+        
+        // Execució Battleship amb la primera versió de codi desenvolupat
+        
         player1Controller.placeShips(shipCount); 
         player2Controller.placeShips(shipCount);
 
@@ -41,9 +45,42 @@ public class main {
         if (player1Hits == shipCount) {
         	System.out.println();
             System.out.println("Player 1 (P1) wins!");
+            System.out.println("P2 ships still alive");
+            for (int i = 1; i <= shipCount - player2Hits; i++) {
+            	System.out.println("Ship "+ i + " still alive");
+            }
+
+        } else {
+        	System.out.println();
+            System.out.println("Player 2 (P2) wins!");
+            System.out.println("P2 ships still alive");
+            for (int i = 1; i <= shipCount - player2Hits ; i++) {
+            	System.out.println("Ship "+ i + " still alive");
+            }
+        }     
+        
+        // Execució Battleship amb la segon versió de codi desenvolupat
+        /*
+        player1Controller.placeShips(shipCount); 
+        player2Controller.placeShips(shipCount);
+
+        int player1Hits2 = 0;
+        int player2Hits2 = 0;
+
+        while (player1Hits2 < shipCount && player2Hits2 < shipCount) {
+            player1Hits2 += player2Controller.takeTurn2(); 
+            if (player1Hits2 == shipCount) break;
+
+            player2Hits2 += player1Controller.takeTurn2(); 
+        }
+
+        if (player1Hits2 == shipCount) {
+        	System.out.println();
+            System.out.println("Player 1 (P1) wins!");
         } else {
         	System.out.println();
             System.out.println("Player 2 (P2) wins!");
         }
+		*/
     }
 }
