dc2600cd99b27153def61627a47faab28a1fac71
PepOrtinLopz <peportinlo@gmail.com>
2024-11-29 09:26:42 +0100

    Update BattleshipControllerTest

--- src/test/java/cat/uab/tqs/controllers/BattleshipControllerTest.java
+++ src/test/java/cat/uab/tqs/controllers/BattleshipControllerTest.java
@@ -58,10 +58,61 @@ class BattleshipControllerTest {
 		assertTrue(messageView3.containsMessage("Cell already occupied. Try again."));
 		
 	}
-
+	
+	@Test
+	void testTakeTurn() {
+		// 1. Tiro con exito 
+		BoardModel board1 = new BoardModel(4, '-', 'S', 'X', 'O');
+		BoardView boardView1 = new BoardView();
+		String name1 = "prova";
+		MockInputView inputView1 = new MockInputView(new int[] {1,1});
+		MockMessageView messageView1 = new MockMessageView();
+		BattleshipController game1 = new BattleshipController(board1, boardView1, inputView1, messageView1, name1);
+		
+		board1.setCell(0, 0, board1.getSize(), 'S');
+		int result1 = game1.takeTurn();
+		
+		assertEquals(1, result1);
+		assertEquals('X',board1.getCell(0, 0));
+		assertTrue(messageView1.containsMessage("Hit!"));
+		
+		// 2. Fallo (casilla vacia)
+		BoardModel board2 = new BoardModel(4, '-', 'S', 'X', 'O');
+		BoardView boardView2 = new BoardView();
+		String name2 = "prova";
+		MockInputView inputView2 = new MockInputView(new int[] {2,2}); 
+		MockMessageView messageView2 = new MockMessageView();
+		BattleshipController game2 = new BattleshipController(board2, boardView2, inputView2, messageView2, name2);
+		
+		int result2= game2.takeTurn();
+		assertEquals(0,result2);
+		assertEquals('-', board2.getCell(0, 0));
+		assertTrue(messageView2.containsMessage("Miss!"));
+		
+		// 3. Casilla ya targeteada
+		BoardModel board3 = new BoardModel(4, '-', 'S', 'X', 'O');
+		BoardView boardView3 = new BoardView();
+		String name3 = "prova";
+		MockInputView inputView3 = new MockInputView(new int[] {1,1,2,2}); //Primer afegim (1,1), despres tornem a intentar afegir (1,1) que no hauria de deixar i afegir 
+		MockMessageView messageView3 = new MockMessageView();
+		BattleshipController game3 = new BattleshipController(board3, boardView3, inputView3, messageView3, name3);
+		
+		board3.setCell(0, 0, board3.getSize(), 'X');
+		int result3 = game3.takeTurn();
+		
+		// 4. Casilla ya fallada
+		BoardModel board4 = new BoardModel(4, '-', 'S', 'X', 'O');
+		BoardView boardView4 = new BoardView();
+		String name4 = "prova";
+		MockInputView inputView4 = new MockInputView(new int[] {1,1,2,2}); //Primer afegim (1,1), despres tornem a intentar afegir (1,1) que no hauria de deixar i afegir 
+		MockMessageView messageView4 = new MockMessageView();
+		BattleshipController game4 = new BattleshipController(board4, boardView4, inputView4, messageView4, name4);
+	}
+	
 	@Test
 	void testStartGame() {
 		fail("Not yet implemented");
 	}
 
 }
+
