4fd0ba8a6736ead523ef4f1b14bf9bba8ed537b0
Arnau Muñoz Barrera <166924717+arnaumunozbarrera@users.noreply.github.com>
2024-11-30 16:39:25 +0100

    Update ShipModel.java

--- src/main/java/cat/uab/tqs/model/ShipModel.java
+++ src/main/java/cat/uab/tqs/model/ShipModel.java
@@ -3,17 +3,30 @@ package main.java.cat.uab.tqs.model;
 public class ShipModel {
 	private int row;
     private int col;
+    
+    private boolean invariant() {
+    	return (row >= 0 && col >= 0);
+    }
 
     public ShipModel(int row, int col) {
+    	
+    	//assert invariant(); // invariant check
+    	
         this.row = row;
         this.col = col;
     }
 
     public int getRow() {
+    	
+    	//assert (row => 0): "Row value too low"; 
+    	
         return row;
     }
 
     public int getCol() {
+    	
+    	//assert (col => 0): "Col value too low"; 
+
         return col;
     }
 }
