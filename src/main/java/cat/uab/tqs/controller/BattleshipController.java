7aa5cdbf18df2fa3b4b92d90c159ade15644373c
PepOrtinLopz <peportinlo@gmail.com>
2024-11-30 13:38:35 +0100

    Update BoardModel

--- src/main/java/cat/uab/tqs/controller/BattleshipController.java
+++ src/main/java/cat/uab/tqs/controller/BattleshipController.java
@@ -1,238 +1,96 @@
-package main.java.cat.uab.tqs.controller;
+package main.java.cat.uab.tqs.model;
 
-import main.java.cat.uab.tqs.model.BoardModel;
-import main.java.cat.uab.tqs.view.BoardView;
-import main.java.cat.uab.tqs.view.InputView;
-import main.java.cat.uab.tqs.view.MessageView;
+import java.util.Arrays;
 
-public class BattleshipController {
-    private final BoardModel board;
-    private final BoardView boardView;
-    private final InputView inputView;
-    private final MessageView messageView;
-    private final String playerName;
+public class BoardModel {
+	private final char[][] grid;
+    private char water;
+    private  char ship;
+    private  char hit;
+    private  char miss;
 
-    public BattleshipController(BoardModel board, BoardView boardView, InputView inputView, MessageView messageView, String playerName) {
-        this.board = board;
-        this.boardView = boardView;
-        this.inputView = inputView;
-        this.messageView = messageView;
-        this.playerName = playerName;
+    public BoardModel(int size, char water, char ship, char hit, char miss) {
+    	//assert(size > 0):"The size of the board must be greater than 0" ;
+        this.grid = new char[size][size];
+        this.water = water;
+        this.ship = ship;
+        this.hit = hit;
+        this.miss = miss;
+        for (char[] row : grid) {
+            Arrays.fill(row, water);
+        }
     }
     
-    private boolean invariant() {
-		return (board.getSize() > 1);
+    private boolean invariant () {
+    	return (grid.length > 1);
     }
 
-    public void placeShips(int shipCount) {
+    public int getSize() {
+    	
+    	//assert invariant (); // invariant check
+    	
+        return grid.length;
+    }
+
+    public char getCell(int row, int col) {
+    	
+    	//assert (row >= 0): "Row value too low"; // precondition
+    	//assert (col >= 0): "Row value too low"; // precondition
+    	
+        return grid[row][col];
+    }
+    
+    public char getWater(int row, int col) {
+    	char c = '-';
+        return c;
+    }
+
+    public boolean setCell(int row, int col, int size, char value) {
     	
     	//assert invariant(); // invariant check
     	
-        messageView.showMessage(playerName + ", place your ships on the board.");
-        messageView.showMessage("Ships available: " + shipCount);
-        //assert (shipCount > 0): "ShipCount value must be greater than 0"; // precondition
+    	if (isBetween(row, size) && isBetween(col, size) && (value == 'X' || value == 'S' || value == 'O' || value == '-')) {
+    		grid[row][col] = value;
+    		return true;
+    	} else {
+    		return false;
+    	}
+    }
 
-        for (int i = 0; i < shipCount; i++) {
-            while (true) {
-            	
-            	
-                int rowPre = inputView.getIntInput(playerName + ", enter row (1-" + board.getSize() + "): ") ;
-                int colPre = inputView.getIntInput(playerName + ", enter column (1-" + board.getSize() + "): ");
-                
-                //assert (rowPre >= 0): "Row value must be greater or equal than 0"; // precondition
-                //assert (rowPre < board.getSize()): "Row value must be lower than board size"; // precondition
-                //assert (colPre >= 0): "Row value must be greater or equal than 0"; // precondition
-                //assert (colPre < board.getSize()): "Row value must be lower than board size"; // precondition
-                
-                int row = rowPre - 1;
-                //assert row == rowPre - 1; // postcondition
-                
-                int col = colPre - 1;
-                //assert col == colPre - 1; // postcondition
+    public boolean isBetween(int coord, int size) {
+    	//assert (size > 0): "Size value too low"; // precondition
+    	//assert (coord >= 0): "Coord value too low"; // precondition
+    	//assert (coord < 5): "Coord value too high"; // precondition
+    	return coord >= 0 && coord < size;
+    }
 
-                System.out.println();
-                
-                //assert (board.getSize() > 1): "Board size value must be greater than 1"; // precondition
+	public boolean isCellEmpty(int row, int col) {
+		//assert (row >= 0): "Row value too low"; // precondition
+    	//assert (row < 5): "Row size too high"; // precondition
+    	//assert (col >= 0): "Row value too low"; // precondition
+    	//assert (col < 5): "Row value too high"; // precondition
+        return grid[row][col] == water;
+    }
 
-                if (row < 0 || row >= board.getSize() || col < 0 || col >= board.getSize()) {
-                    messageView.showMessage("Coordinates out of bounds. Try again.");
-                } else if (!board.isCellEmpty(row, col)) {
-                    messageView.showMessage("Cell already occupied. Try again.");
-                } else {
-                    board.setCell(row, col,board.getSize(), board.getShipChar());
-                    //assert (board.getCell(row, col) != ' '): "Board cell should not be empty"; // postcondition
-                    break;
-                }
-                
-                /*if (!board.isCellEmpty(row, col)) {
-                    messageView.showMessage("Cell already occupied. Try again.");
-                } else if (!board.setCell(row, col, board.getSize(), board.getShipChar())) {
-                    messageView.showMessage("Coordinates out of bounds. Try again.");
-                } else {
-                    break;
-                }*/
-
-                
-            }
-        }
+    public char[][] getGrid() {
+    	//assert (grid.length > 1): "Grid length too small"; // precondition
+    	//assert (grid.length < 5): "Grid length too high"; // precondition
+        return grid;
     }
     
-    public void placeShips2(int shipCount) {
-
-        //assert invariant(); // invariant check
-
-        messageView.showMessage(playerName + ", place your ships on the board.");
-        messageView.showMessage("Ships available: " + shipCount);
-        //assert (shipCount > 0) : "ShipCount value must be greater than 0"; // precondition
-
-        int shipsPlaced = 0;
-
-        while (shipsPlaced < shipCount) {
-            while (true) {
-                int rowPre = inputView.getIntInput(playerName + ", enter row (1-" + board.getSize() + "): ");
-                int colPre = inputView.getIntInput(playerName + ", enter column (1-" + board.getSize() + "): ");
-
-                //assert (rowPre >= 0) : "Row value must be greater or equal than 0"; // precondition
-                //assert (rowPre < board.getSize()) : "Row value must be lower than board size"; // precondition
-                //assert (colPre >= 0) : "Column value must be greater or equal than 0"; // precondition
-                //assert (colPre < board.getSize()) : "Column value must be lower than board size"; // precondition
-
-                int row = rowPre - 1;
-                int col = colPre - 1;
-
-                //assert row == rowPre - 1; // postcondition
-                //assert col == colPre - 1; // postcondition
-
-                //assert (board.getSize() > 1) : "Board size value must be greater than 1"; // precondition
-
-                String validation;
-                if (row < 0 || row >= board.getSize() || col < 0 || col >= board.getSize()) {
-                    validation = "OUT_OF_BOUNDS";
-                } else if (!board.isCellEmpty(row, col)) {
-                    validation = "OCCUPIED";
-                } else {
-                    validation = "VALID";
-                }
-
-                switch (validation) {
-                    case "OUT_OF_BOUNDS":
-                        messageView.showMessage("Coordinates out of bounds. Try again.");
-                        break;
-                    case "OCCUPIED":
-                        messageView.showMessage("Cell already occupied. Try again.");
-                        break;
-                    case "VALID":
-                        board.setCell(row, col, board.getSize(), board.getShipChar());
-                        //assert (board.getCell(row, col) != ' ') : "Board cell should not be empty"; // postcondition
-                        shipsPlaced++;
-                        break;
-                }
-
-                if (validation.equals("VALID")) break; 
-            }
-        }
-    }
-    
-    public int takeTurn() {
-        messageView.showMessage(playerName + ", it's your turn.");
-        
-        //assert invariant(); // invariant check
-        
-        boardView.printBoard(board, true);
-
-        while (true) {
-            int rowPre = inputView.getIntInput("Enter row: ");
-            int colPre = inputView.getIntInput("Enter column: ");
-            
-            //assert (rowPre >= 0): "Row value must be greater or equal than 0"; // precondition
-            //assert (rowPre < board.getSize()): "Row value must be lower than board size"; // precondition
-            //assert (colPre >= 0): "Row value must be greater or equal than 0"; // precondition
-            //assert (colPre < board.getSize()): "Row value must be lower than board size"; // precondition
-            
-            int row = rowPre - 1;
-            //assert row == rowPre - 1; // postcondition
-            
-            int col = colPre - 1;
-            //assert col == colPre - 1; // postcondition
-
-            if (row < 0 || row >= board.getSize() || col < 0 || col >= board.getSize()) {
-                messageView.showMessage("Coordinates out of bounds. Try again.");
-            } else {
-                char target = board.getCell(row, col);
-                if (target == board.getShipChar()) {
-                    board.setCell(row, col, board.getSize(),board.getHitChar());
-                    messageView.showMessage("Hit!");
-                    //assert (board.getCell(row, col) == 'X'): "Board cell should not be hitted"; // postcondition
-                    return 1; // Hit success
-                } else if (target == board.getHitChar() || target == board.getMissChar()) {
-                    messageView.showMessage("Already targeted. Try again.");
-                    //assert (board.getCell(row, col) == 'X' || board.getCell(row, col) == 'O'): "Board cell must be already used"; // postcondition
-                } else {
-                    board.setCell(row, col,board.getSize(), board.getMissChar());
-                    messageView.showMessage("Miss!");
-                    //assert (board.getCell(row, col) == 'O'): "Board cell should be missed"; // postcondition
-                    return 0; // Miss
-                }
-            }
-        }
-    }
-    
-    public int takeTurn2() {
-        messageView.showMessage(playerName + ", it's your turn.");
-
-        //assert invariant(); // invariant check
-
-        boardView.printBoard(board, false);
-
-        while (true) {
-            int rowPre = inputView.getIntInput("Enter row: ");
-            int colPre = inputView.getIntInput("Enter column: ");
-
-            //assert (rowPre >= 0) : "Row value must be greater or equal than 0"; // precondition
-            //assert (rowPre < board.getSize()) : "Row value must be lower than board size"; // precondition
-            //assert (colPre >= 0) : "Column value must be greater or equal than 0"; // precondition
-            //assert (colPre < board.getSize()) : "Column value must be lower than board size"; // precondition
-
-            int row = rowPre - 1;
-            int col = colPre - 1;
-
-            //assert row == rowPre - 1; // postcondition
-            //assert col == colPre - 1; // postcondition
-
-            String action;
-            if (row < 0 || row >= board.getSize() || col < 0 || col >= board.getSize()) {
-                action = "OUT_OF_BOUNDS";
-            } else {
-                char target = board.getCell(row, col);
-                if (target == board.getShipChar()) {
-                    action = "HIT";
-                } else if (target == board.getHitChar() || target == board.getMissChar()) {
-                    action = "ALREADY_TARGETED";
-                } else {
-                    action = "MISS";
-                }
-            }
-
-            switch (action) {
-                case "OUT_OF_BOUNDS":
-                    messageView.showMessage("Coordinates out of bounds. Try again.");
-                    break;
-                case "HIT":
-                    board.setCell(row, col, board.getSize(), board.getHitChar());
-                    messageView.showMessage("Hit!");
-                    //assert (board.getCell(row, col) == board.getHitChar()) : "Board cell should be marked as hit"; // postcondition
-                    return 1; // Hit success
-                case "ALREADY_TARGETED":
-                    messageView.showMessage("Already targeted. Try again.");
-                    //assert (board.getCell(row, col) == board.getHitChar() || board.getCell(row, col) == board.getMissChar()) : "Board cell must already be used"; // postcondition
-                    break;
-                case "MISS":
-                    board.setCell(row, col, board.getSize(), board.getMissChar());
-                    messageView.showMessage("Miss!");
-                    //assert (board.getCell(row, col) == board.getMissChar()) : "Board cell should be marked as missed"; // postcondition
-                    return 0; // Miss
-            }
-        }
+    public boolean isValidChar(char value) {
+    	return value == water || value == ship || value == hit || value == miss;
     }
 
+    public char getShipChar() {
+        return ship;
+    }
+
+    public char getHitChar() {
+        return hit;
+    }
+
+    public char getMissChar() {
+        return miss;
+    }
 }
